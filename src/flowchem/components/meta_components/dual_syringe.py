e4bafe6c73f0cf38c987ad190a06f38c2719484a
magz97 <miguelgarciazubillaga@gmail.com>
2025-02-27 10:07:09 +0100

    changed logic to use ureg.Quantity in syringe volumes making it easier to modify when calling functions

--- src/flowchem/components/meta_components/dual_syringe.py
+++ src/flowchem/components/meta_components/dual_syringe.py
@@ -1,49 +1,48 @@
 from time import sleep
+from loguru import logger
 from flowchem import ureg
 from flowchem.utils.exceptions import InvalidConfigurationError
 from flowchem.client.component_client import FlowchemComponentClient
-
 
 class DualSyringe:
     """
     Autosampler meta components.
     """
-    def __init__(self, left_pump=None, right_pump=None, left_valve=None, right_valve=None, syringe_volumes: dict | str | None = None):
+    def __init__(self, left_pump=None, right_pump=None, left_valve=None, right_valve=None, syringe_volume: ureg.Quantity | dict[str, ureg.Quantity] | None = None):
         self.left_pump: FlowchemComponentClient = left_pump
         self.right_pump: FlowchemComponentClient = right_pump
         self.left_valve: FlowchemComponentClient = left_valve
         self.right_valve: FlowchemComponentClient = right_valve
-        if type(syringe_volumes) is str:
-            self.syringe_volumes = {"left": ureg.Quantity(syringe_volumes), "right": syringe_volumes}
-        elif isinstance(syringe_volumes, dict) and all(isinstance(key, str) for key in syringe_volumes.keys()):
-            self.syringe_volumes = {"left": syringe_volumes["left"], "right": syringe_volumes["right"]}
+        if type(syringe_volume) is ureg.Quantity:
+            self.syringe_volumes = {"left": syringe_volume, "right": syringe_volume}
+        elif isinstance(syringe_volume, dict) and all(isinstance(key, ureg.Quantity) for key in syringe_volume.keys()):
+            self.syringe_volume = {"left": syringe_volume["left"], "right": syringe_volume["right"]}
         else:
             raise InvalidConfigurationError("Please provide valid syringe volumes as a single str (ex: '5 ml') or a dict"
                                             "  ({'left': '5 ml', 'right': '1 ml'})")
 
-    def fill_dual_syringes(self, to_volume_left: float = None, speed_left: float = None,
-                                   to_volume_right: float = None, speed_right: float = None):
+    def fill_dual_syringes(self, to_volume_left: ureg.Quantity = None, speed_left: ureg.Quantity = None,
+                                   to_volume_right: ureg.Quantity = None, speed_right: ureg.Quantity = None):
         """
         Assumes Input on left of valve and output on the right
         """
+        if to_volume_right is None and speed_right is None and to_volume_left is not None and speed_left is not None:
+            to_volume_right = to_volume_left
+            speed_right = speed_left
         # switch valves
-        assert self.syringe_volumes["left"] == self.syringe_volumes[
+        assert self.syringe_volume["left"] == self.syringe_volume[
             "right"], "Syringes are not the same size, this can create unexpected behaviour"
-        while True:
-            if not self.left_pump.get("is_pumping") and not self.right_pump.get("is_pumping"):
-                self.left_valve.put("set_position", {"connect": "[[null,0],[2,3]]"})
-                self.right_valve.put("set_position", {"connect": "[[null,0],[2,3]]"})
-                #self.wait_until_idle(syringe=None)
-                # ToDo Check if it's necessary to get valve's status (is_idle)
-                # actuate syringes
-                self.left_pump.put("set_to_volume", {"target_volume": to_volume_left, "rate": speed_left})
-                self.right_pump.put("set_to_volume", {"target_volume": to_volume_right, "rate": speed_right})
-                break
-            else:
-                sleep(0.01)
+        self.wait_until_idle()
+        self.left_valve.put("set_position", {"connect": "[[null,0],[2,3]]"})
+        self.right_valve.put("set_position", {"connect": "[[null,0],[2,3]]"})
+        #self.wait_until_idle(syringe=None)
+        # ToDo Check if it's necessary to get valve's status (is_idle)
+        # actuate syringes
+        self.left_pump.put("set_to_volume", {"target_volume": str(to_volume_left), "rate": str(speed_left)})
+        self.right_pump.put("set_to_volume", {"target_volume": str(to_volume_right), "rate": str(speed_right)})
 
-    def deliver_from_dual_syringes(self, to_volume_left: float = None, speed_left: float = None,
-                                   to_volume_right: float = None, speed_right: float = None):
+    def deliver_from_dual_syringes(self, to_volume_left: ureg.Quantity = None, speed_left: ureg.Quantity = None,
+                                   to_volume_right: ureg.Quantity = None, speed_right: ureg.Quantity = None):
         """
         Assumes Input on left of valve and output on the right
 
@@ -54,7 +53,10 @@ class DualSyringe:
         Returns:
 
         """
-        assert self.syringe_volumes["left"] == self.syringe_volumes[
+        if to_volume_right is None and speed_right is None and to_volume_left is not None and speed_left is not None:
+            to_volume_right = to_volume_left
+            speed_right = speed_left
+        assert self.syringe_volume["left"] == self.syringe_volume[
             "right"], "Syringes are not the same size, this can create unexpected behaviour"
         # switch valves
         self.wait_until_idle()
