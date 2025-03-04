e023ed8c0f3e70ae8b7188a08dbd8060f06ec186
magz97 <miguelgarciazubillaga@gmail.com>
2025-03-04 16:37:22 +0100

    ml600 used str for set_raw_position

--- src/flowchem/devices/hamilton/ml600_valve.py
+++ src/flowchem/devices/hamilton/ml600_valve.py
@@ -23,32 +23,31 @@ class ML600LeftValve(FourPortFivePositionValve):
     # 315
     # 360
 
-    def _change_connections(self, raw_position, reverse: bool = False) -> int:
+    def _change_connections(self, raw_position: str, reverse: bool = False) -> str:
         if not reverse:
-            translated = raw_position * 45
+            translated = int(raw_position) * 45
         else:
-            translated = round(raw_position / 45)
-        return translated
-
+            translated = round(int(raw_position) / 45)
+        return str(translated)
 
 class ML600RightValve(ThreePortFourPositionValve):
     hw_device: ML600  # for typing's sake
     identifier = "C"
 
-    def _change_connections(self, raw_position, reverse: bool = False) -> int:
+    def _change_connections(self, raw_position: str, reverse: bool = False) -> str:
         if not reverse:
-            translated = (raw_position + 2) * 90
+            translated = (int(raw_position) + 2) * 90
             if translated >= 360:
                 translated -= 360
         else:
             # round, the return is often off by 1°/the valve does not switch exactly
             # the slightly complicated logic here is because the degrees are differently defined in the abstract valve
             # and the physical ML600 valve, the offset in multiples of 90 degres is corrected here
-            translated = round(raw_position / 90)
+            translated = round(int(raw_position) / 90)
             if translated < 2:
                 translated += 2
             else:
                 translated -= 2
 
-        return translated
+        return str(translated)
 
