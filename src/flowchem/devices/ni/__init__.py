06d84baca4ac3bd712052cc509af61a54f731445
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2026-06-11 14:56:26 +0200

    feat: Add NI USB-TC01 thermocouple sensor support

--- src/flowchem/devices/ni/__init__.py
+++ src/flowchem/devices/ni/__init__.py
@@ -2,6 +2,7 @@
 
 from .ni6519 import NI6519
 from .ni9477 import NI9477
+from .ni_usbtc01 import NIUSBTC01
 from .nidaq_analog_io import NIDAQAnalogIO
 
-__all__ = ["NI6519", "NI9477", "NIDAQAnalogIO"]
+__all__ = ["NI6519", "NI9477", "NIUSBTC01", "NIDAQAnalogIO"]
